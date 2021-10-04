1aab258baefeacdf482664117973a69867565db0
David Alejandro García Hernádez - Ceiba Software <david.garcia@ceiba.com.co>
2021-10-04 07:40:08 -0500

    fix broken files

--- src/app/feature/desayuno/components/boton-comprar-desayuno/boton-comprar-desayuno.component.ts
+++ src/app/feature/desayuno/components/boton-comprar-desayuno/boton-comprar-desayuno.component.ts
@@ -2,8 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-boton-comprar-desayuno',
-    templateUrl: './boton-comprar-desayuno.component.html',
-    styleUrls: ['./boton-comprar-desayuno.component.css']
+    templateUrl: './boton-comprar-desayuno.component.html'
   })
 export class BotonComprarDesayunoComponent {
   @Input() link: string;
